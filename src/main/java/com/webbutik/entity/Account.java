38821a042d7697a2be9a97897e4da7b480e82a51
ERIK\Erik <eriksandstrom@outlook.com>
2021-01-29 02:29:29 +0100

    add account entity

---
+++ src/main/java/com/webbutik/entity/Account.java
@@ -0,0 +1,85 @@
+package com.webbutik.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+* 
+* Represents a user's account. 
+* <p>
+* @author Erik Sandström
+* </p>
+*/
+@Entity
+@Table(name = "Account")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Account implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	public static final int CUSTOMER = 0;
+	public static final int ADMIN = 1;
+	
+	private String firstName;
+	private String surname;
+	@Column(unique = true, nullable = false)
+	private String email;
+	private String password;
+	private int accessLevel;
+	private boolean premiumMember = false;
+	
+	public Account(String firstName, String surname, String email, String password) {
+		this.firstName = firstName;
+		this.surname = surname;
+		this.email = email;
+		this.password = password;
+		this.accessLevel = CUSTOMER;
+		this.premiumMember = false;
+	}
+	
+	public Account() {
+		
+	}
+	
+	public String getFullName() {
+		return this.firstName + " " + this.surname;
+	}
+	
+	public boolean verifyPassword(String password) {
+		return (this.password == password) ? true : false;
+	}
+	
+	public boolean verifyAccessLevel() {
+		return(this.accessLevel == ADMIN) ? true : false;
+	}
+	
+	public boolean verifyMembership() {
+		return(this.premiumMember) ? true : false;
+	}
+	
+	public String toString() {
+		return "Name: " + firstName + " " + surname + "   Email:" + email;
+	}
+
+}
